74e19562f5fbc90f58d23aba5c6406116be1c295
Dan Chan <dcyc88@gmail.com>
2018-08-26 13:00:11 +0800

    13-securityBreach

---
+++ arcade/databases/13-securityBreach.sql
@@ -0,0 +1,8 @@
+/*Please add ; after each select statement*/
+CREATE PROCEDURE securityBreach()
+BEGIN
+	SELECT *
+    FROM users
+    WHERE attribute LIKE BINARY concat("%_\%", first_name, "_", second_name, "\%%")
+    ORDER BY attribute ASC;
+END
